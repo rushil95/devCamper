6c224d824d2cb5a8365c9597513aeeb3e578a236
rushil95 <rushild95@gmail.com>
2020-11-27 16:14:34 +0530

    Connected to db Atlas

--- server.js
+++ server.js
@@ -1,18 +1,28 @@
 const express = require("express");
 const dotenv = require("dotenv");
-
-//Load bootcamp routes
-const bootcamps = require('./routes/bootcamps')
+const colors = require("colors")
+const connectDB = require("./config/db")
 
 //Load env variables
 dotenv.config({ path: "./config/config.env" });
 
+//Load bootcamp routes
+const bootcamps = require('./routes/bootcamps');
+
+connectDB()
 const app = express();
 app.use('/api/v1/bootcamps',bootcamps)
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
 );
+
+//Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  // Close server and exit proccess
+  server.close( ()=> process.exit(1))
+})
